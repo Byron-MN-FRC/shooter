8b72bf44a0df89990ee2f97f8de9480f7a222538
bdilwo50 <bdilwo50@students.byron.k12.mn.us>
2020-02-01 10:32:37 -0600

    Code to limit current and read velocity

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+
+
+public class Constants {
+
+//Variables for current limiting on shooter motor
+
+//Enable or disable current limitor
+public static final boolean enable = true;
+
+//Current (amp) limit
+public static final double currentLimit = 30;
+
+//Threshold that must be exceeded for current limiting to occur
+public static final double thresholdLimit = 20;
+
+//How long the current has to be above the threshold to trigger limiting
+public static final double thresholdTime = 0;
+
+
+
+
+}
